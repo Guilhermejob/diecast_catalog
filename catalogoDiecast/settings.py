fe1819ac594b078292036fe3dd42c152eb95472d
Guilherme Armesto Job <guilhermejob047@gmail.com>
2022-09-11 16:19:38 -0300

    final settings

--- catalogoDiecast/settings.py
+++ catalogoDiecast/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +28,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Initialise environment variables
+
+env = environ.Env()
+
+environ.Env.read_env()
 
 # Application definition
 
@@ -83,8 +89,12 @@ WSGI_APPLICATION = 'catalogoDiecast.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
